6432f8201e8cf6ed5c5c9c47168b54bdbb8d5256
saito <saito@example.com>
2020-02-13 11:17:03 +0900

    pollsにstaticを追加

--- kakeibosite/settings.py
+++ kakeibosite/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kakeibo',
         'USER': 'kakeibo',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
     }
